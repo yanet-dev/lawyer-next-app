c8451065671257fa4552078b84a1d2a1d58ca665
Yanetdago <yanet.dago95@gmail.com>
2022-11-29 00:05:39 +0200

    fix styles and convert pixels to rem

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -19,21 +19,21 @@ function Footer() {
                                 <div className="col-md-2">
                                     <Link href="https://www.facebook.com/RetterLaw/" legacyBehavior>
                                         <a target="_blank" className={`${styles.footerSocialMedia}`}>
-                                            <i className={`${styles.footerSocialIcon} bi bi-facebook `}></i>
+                                            <i className={`${styles.footerSocialIcon} bi bi-facebook `}/>
                                         </a>
                                     </Link>
                                 </div>
                                 <div className="col-md-2">
                                     <Link href="/" legacyBehavior>
                                         <a target="_blank" className={`${styles.footerSocialMedia}`}>
-                                            <i className={`${styles.footerSocialIcon} bi bi-instagram `}></i>
+                                            <i className={`${styles.footerSocialIcon} bi bi-instagram `}/>
                                         </a>
                                     </Link>
                                 </div>
                                 <div className="col-md-2">
                                     <Link href="/" legacyBehavior>
                                         <a target="_blank" className={`${styles.footerSocialMedia}`}>
-                                            <i className={`${styles.footerSocialIcon} bi bi-linkedin `}></i>
+                                            <i className={`${styles.footerSocialIcon} bi bi-linkedin `}/>
                                         </a>
                                     </Link>
                                 </div>
@@ -51,31 +51,30 @@ function Footer() {
                         <h4 className={`${styles.headingLabel}`}>לראשונה בישראל</h4>
                         <div>
                             <a href="#" className={`${styles.footerSocialMedia}`}>
-                                <i className={`${styles.footerSocialIcon} bi bi-geo-alt-fill`}></i>
+                                <i className={`${styles.footerSocialIcon} bi bi-geo-alt-fill`}/>
                             </a>
                             <span className="m-3">טקסט לורם איפסום בעברית לטובת</span>
                         </div>
                         <div>
                             <a href="#" className={`${styles.footerSocialMedia}`}>
-                                <i className={`${styles.footerSocialIcon} bi bi-telephone-fill`}></i>
+                                <i className={`${styles.footerSocialIcon} bi bi-telephone-fill`}/>
                             </a>
                             <span className="m-3">טקסט לורם איפסום בעברית לטובת</span>
                         </div>
 
                         <div>
                             <a href="#" target="_blank" className={`${styles.footerSocialMedia}`}>
-                                <i className={`${styles.footerSocialIcon} bi bi-envelope-fill`}></i>
+                                <i className={`${styles.footerSocialIcon} bi bi-envelope-fill`}/>
                             </a>
                             <span className="m-3">טקסט לורם איפסום בעברית לטובת</span>
                         </div>
                     </div>
                 </div>
-                <hr className={`${styles.footerMiddleLine}`}/>
+                <hr/>
                 <div className="row d-flex justify-content-around">
                     <div className="col-md-6 text-center">
                         <h6> coppyright @ לורם איפסום הוא טקסט חסר הקשר וחסר</h6>
                     </div>
-
                 </div>
             </div>
         </footer>
--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -6,9 +6,15 @@ import styles from "/styles/Navigation.module.scss";
 function Navigation() {
     return (
         <div className="container-fluid">
-            <nav className={`${styles.header} navbar navbar-expand-lg`}>
+            <nav className={`${styles.navBar} navbar navbar-expand-lg`}>
                 <div className="container">
-                    <div className="collapse navbar-collapse align-items-center" id="navbarNav">
+                    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <a className={`${styles.contactButton} btn btn-primary`} type="button">
+                            <i className={`${styles.contactIcon} bi bi-arrow-right-circle-fill`}/>
+                            Contact Me
+                        </a>
+                    </div>
+                    <div className="collapse navbar-collapse d-flex justify-content-around">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -29,7 +35,7 @@ function Navigation() {
                     </div>
                     <Link href="/" legacyBehavior>
                         <a className="navbar-brand">
-                            <Image src="/scale.png" alt="mithabrim laneshama" width={50} height={40}/>
+                            <Image src="/scale.png" alt="logo" width={50} height={40}/>
                         </a>
                     </Link>
                 </div>
